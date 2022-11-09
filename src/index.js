cbe76e1766834ea43f699e7af6973306e11c940a
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2022-11-09 12:53:20 +0300

    Подключение js (исследование простого кода) 1. Подключили js 2. Запросили у пользователя ввод 3 значений 3. Поместили значения в переменные (operand1...) 4. Вывели результат на консоль

---
+++ src/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Задача запросить у пользователя
+// 3 числа и найти наибольшее среди чисел
+
+let operand1 = Number(prompt("Введите 1 число: "));
+let operand2 = Number(prompt("Введите 2 число: "));
+let operand3 = Number(prompt("Введите 3 число: "));
+
+// Обращаем внимание на функцию "Number" -
+// перевод значения в поле на число!
+
+// Обращаем внимание на вывод в консоли
+console.log(operand1, operand2, operand3);
+
+console.log("operand1", "operand2", "operand3");
